096a956c2dad6a88316634b09684a446ba91f922
Timofei Ganjusev <timofei.ganjusev@lhv.ee>
2024-04-03 03:35:35 +0300

    Added PriorityQueueService

--- service-order-queue/src/main/java/com/bookstore/OrderQueueService.java
+++ service-order-queue/src/main/java/com/bookstore/OrderQueueService.java
@@ -4,15 +4,21 @@ import com.dspractice.bookstore.commonproto.OrderEnqueueRequest;
 import com.dspractice.bookstore.commonproto.OrderEnqueueResponse;
 import com.dspractice.bookstore.commonproto.OrderQueueServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @Slf4j
 @GrpcService
+@RequiredArgsConstructor
 public class OrderQueueService extends OrderQueueServiceGrpc.OrderQueueServiceImplBase {
+
+    private final PriorityQueueService priorityQueueService;
+
     @Override
     public void enqueueOrder(OrderEnqueueRequest request, StreamObserver<OrderEnqueueResponse> responseObserver) {
         log.info("[Order ID: {}] {}", request.getId(), "Waiting enqueue");
+        priorityQueueService.enqueueOrder(request);
         OrderEnqueueResponse response = OrderEnqueueResponse.newBuilder()
                 .setOrderId(request.getId())
                 .setStatus("Enqueued")
